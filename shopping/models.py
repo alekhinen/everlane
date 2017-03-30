b42c39e151fefb2783e82928f34b9336a4201978
Nick Alekhine <nick.alekhine@gmail.com>
2017-03-29 21:24:18 -0400

    adds in Product model.

--- shopping/models.py
+++ shopping/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """
+        A product that can be purchased.
+    """
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    available_inventory = models.IntegerField(null=True, blank=True)
